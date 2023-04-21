ace1cdf343d25916ac2d763bc6b09029c3df4348
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-04-21 17:58:18 +0530

    v-6 Time Series

---
+++ src/components/timeSeries/TimesSeries.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const TimesSeries = () => {
+    return (
+        <div>
+            <h2>TimesSeries</h2>
+            <Link to='/'>Go Back Home</Link>
+        </div>
+    )
+}
+
+export default TimesSeries
